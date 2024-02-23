7ad60937125409c996f0a127846b4746a2003576
ChenYujunjks <aaaeeettt124@126.com>
2024-02-23 16:47:39 -0500

    finish experiment

--- Experiments/netExperiments/Server.mjs
+++ Experiments/netExperiments/Server.mjs
@@ -2,21 +2,21 @@ import net from 'net';
 
 const server = net.createServer();
 
-server.on('connection', clientsocket => {
+server.on('connection', csocket => {
     console.log('客户端已连接');
 
-    clientsocket.on('data', data => {
+    csocket.on('data', data => {
         console.log('收到数据：', data.toString());
-        clientsocket.write('你好，客户端！\r\n （这是一条从服务器发给客户端的信息）');
+        csocket.write('你好，客户端！\r\n （这是一条从服务器发给客户端的信息）');
       });
 
-    clientsocket.on('end', () => {
+    csocket.on('end', () => {
         console.log('Client disconnected');
     });
-    clientsocket.on('close', () => {
+    csocket.on('close', () => {
         console.log('连接已关闭');
     });
-    //clientsocket.end('Server closing connection.\r\n');
+    //csocket.end('Server closing connection.\r\n');
 });
 
 server.listen(12345, () => {
