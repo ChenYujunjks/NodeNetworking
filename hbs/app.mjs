6c1df28a6b9ca85a123147fff5b195ec55f56f31
ChenYujunjks <aaaeeettt124@126.com>
2024-02-28 18:02:49 -0500

    mail

--- hbs/app.mjs
+++ hbs/app.mjs
@@ -15,7 +15,7 @@ app.use(function(req, res, next) {
 	next();
 });
 app.use(function(req, res, next) {
-	console.log('hello');
+	console.log('hello---------------');
 	next();
 });
 app.use(function(req, res, next) {
@@ -33,12 +33,21 @@ app.get('/divide', (req, res) => {
    res.render('divisionForm');
 })
 app.post('/divide', (req, res) => {
-   console.log(req.body, 'req:', req)
+   if (!req.body){
+      res.render('divisionForm');
+      return
+   }
+   //console.log(req.body, 'req:', req)
    const { numerator, denominator } = req.body;
+   if (denominator === '0') {
+      //Handle divisionForm by zero error, optionally
+      res.render('divisionForm', {error: 'Cannot divide by zero.'});
+      return;
+   }
    const result = numerator / denominator;
    //res.send('youma ')
-   res.send(`${numerator}`+'/'+`${denominator}`+ '=' + `${result}`)
-})
+   res.render('divisionForm', {result: `${numerator} / ${denominator} = ${result}`});
+});
 
 app.listen(port, () => {
    console.log(`Example app listening at http://localhost:${port}`);
