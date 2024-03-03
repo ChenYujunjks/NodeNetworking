d4a92f33a455de932ecacf7edcf5b9e85d7f196c
Bruce Suade <yc5508@nyu.edu>
2024-03-03 01:23:53 -0500

    new database

---
+++ db_try/jddmx.mjs
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+mongoose.connect('mongodb://localhost/mydatabase');
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
